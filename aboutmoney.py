8640520c7d3950eb3908a6b68255583610601e5a
elie1381999 <eliebousleiman14@gmail.com>
2025-08-24 04:36:49 +0300

    Update aboutmoney.py

--- aboutmoney.py
+++ aboutmoney.py
@@ -3,7 +3,7 @@ from venv import logger
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes, ConversationHandler
 from datetime import datetime, date, timedelta
-from farmcore import farm_core
+from farmcore import farm_core, init_farm_core
 from keyboards import get_main_keyboard
 
 EXPENSE_STATES = {
@@ -544,3 +544,4 @@ async def weekly_summary(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         )
 
         await send(short, reply_markup=get_main_keyboard(lang))
+
