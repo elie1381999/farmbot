d029e6be3f41c9f973c53c6136b817833e33f905
elie1381999 <eliebousleiman14@gmail.com>
2025-08-24 04:13:49 +0300

    Update core_singleton.py

--- core_singleton.py
+++ core_singleton.py
@@ -1,9 +1,4 @@
-"""
-Singleton holder for FarmCore. The module exposes:
-- farm_core: set to None initially, will hold FarmCore instance after init_farm_core()
-- init_farm_core(supabase_url, supabase_key): initialize farm_core once
-"""
-
+'''
 from typing import Optional
 from farmcore import FarmCore
 
@@ -13,3 +8,5 @@ def init_farm_core(supabase_url: str, supabase_key: str) -> None:
     global farm_core
     if farm_core is None:
         farm_core = FarmCore(supabase_url=supabase_url, supabase_key=supabase_key)
+
+'''
